3535d45740bb7fdf14e02bb190de31c436b32bd0
Dmitry <adichev17@gmail.com>
2022-06-17 23:42:47 +0300

    Composite pattern. code review

--- PattersTests/CompositeTests.cs
+++ PattersTests/CompositeTests.cs
@@ -1,11 +1,7 @@
 ﻿using NUnit.Framework;
 using Patterns.Composite;
 using Patterns.Composite.Models;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PattersTests
 {
