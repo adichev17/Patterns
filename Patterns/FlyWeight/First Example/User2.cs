bdd985130c43189624949af10c8bf7e7b89de409
Dmitry <adichev17@gmail.com>
2022-06-20 23:16:07 +0300

    Flyweight pattern

---
+++ Patterns/FlyWeight/First Example/User2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.FlyWeight.First_Example
+{
+    public class User2
+    {
+        static List<string> strings = new List<string>();
+        private int[] names;
+
+        public static List<string> Strings = strings;
+
+        public User2(string fullName)
+        {
+            int getOrAdd(string s)
+            {
+                int idx = strings.IndexOf(s);
+                if (idx != -1) return idx;
+                else
+                {
+                    strings.Add(s);
+                    return strings.Count - 1;
+                }
+            }
+
+            names = fullName.Split(' ').Select(getOrAdd).ToArray();
+        }
+
+        public string FullName => string.Join(" ", names.Select(i => strings[i]));
+    }
+}
---
+++ PattersTests/FlyweightTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Patterns.FlyWeight.First_Example;
+using System.Collections.Generic;
+
+namespace PattersTests
+{
+    [TestFixture]
+    public class FlyweightTests
+    {
+        [Test]
+        public void TestUser2()
+        {
+            var firstUser = new User2("Nick Diaz");
+            var secondUser = new User2("Tom Diaz");
+
+            Assert.That(User2.Strings.Count, Is.EqualTo(3));
+        }
+    }
+}
