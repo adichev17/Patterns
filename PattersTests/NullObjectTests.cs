8e51e5a726aeb2de0dcecebfbc3fddb897596fb8
Dmitry <adichev17@gmail.com>
2022-07-10 17:35:06 +0300

    NullObject

---
+++ PattersTests/NullObjectTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Patterns.NullObject.Example;
+
+namespace PattersTests
+{
+    [TestFixture]
+    public class NullObjectTests
+    {
+        [Test]
+        public void Test()
+        {
+            var log = new NullLog();
+            var ba = new BankAccount(log);
+            ba.Deposit(100);
+            ba.Withdraw(200);
+        }
+    }
+}
