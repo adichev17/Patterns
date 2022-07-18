b4d0c8f30b9f021fa637f077ccd28bd30a88c695
Dmitry <adichev17@gmail.com>
2022-07-18 21:39:54 +0300

    Visitor

---
+++ Patterns/Visitor/Double Dispatch/ExampleDoubleDispatch.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.Visitor.Double_Dispatch
+{
+    public abstract class ExpressionVisitor
+    {
+        public abstract void Visit(Value value);
+        public abstract void Visit(AdditionExpression ae);
+        public abstract void Visit(MultiplicationExpression me);
+    }
+
+    public abstract class Expression
+    {
+        public abstract void Accept(ExpressionVisitor ev);
+    }
+
+    public class Value : Expression
+    {
+        public readonly int TheValue;
+
+        public Value(int value)
+        {
+            TheValue = value;
+        }
+
+        public override void Accept(ExpressionVisitor ev)
+        {
+            ev.Visit(this);
+        }
+    }
+
+    public class AdditionExpression : Expression
+    {
+        public readonly Expression LHS, RHS;
+
+        public AdditionExpression(Expression lhs, Expression rhs)
+        {
+            LHS = lhs;
+            RHS = rhs;
+        }
+
+        public override void Accept(ExpressionVisitor ev)
+        {
+            ev.Visit(this);
+        }
+    }
+
+    public class MultiplicationExpression : Expression
+    {
+        public readonly Expression LHS, RHS;
+
+        public MultiplicationExpression(Expression lhs, Expression rhs)
+        {
+            LHS = lhs;
+            RHS = rhs;
+        }
+
+        public override void Accept(ExpressionVisitor ev)
+        {
+            ev.Visit(this);
+        }
+    }
+
+    public class ExpressionPrinter : ExpressionVisitor
+    {
+        private StringBuilder sb = new StringBuilder();
+
+        public override void Visit(Value value)
+        {
+            sb.Append(value.TheValue);
+        }
+
+        public override void Visit(AdditionExpression ae)
+        {
+            sb.Append("(");
+            ae.LHS.Accept(this);
+            sb.Append("+");
+            ae.RHS.Accept(this);
+            sb.Append(")");
+        }
+
+        public override void Visit(MultiplicationExpression me)
+        {
+            me.LHS.Accept(this);
+            sb.Append("*");
+            me.RHS.Accept(this);
+        }
+
+        public override string ToString()
+        {
+            return sb.ToString();
+        }
+    }
+}
---
+++ PattersTests/VisitorTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using Patterns.Visitor.Double_Dispatch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PattersTests
+{
+    [TestFixture]
+    public class VisitorTests
+    {
+        [Test]
+        public void SimpleAddition()
+        {
+            var simple = new AdditionExpression(new Value(2), new Value(3));
+            var ep = new ExpressionPrinter();
+            ep.Visit(simple);
+            Assert.That(ep.ToString(), Is.EqualTo("(2+3)"));
+        }
+
+        [Test]
+        public void ProductOfAdditionAndValue()
+        {
+            var expr = new MultiplicationExpression(
+              new AdditionExpression(new Value(2), new Value(3)),
+              new Value(4)
+              );
+            var ep = new ExpressionPrinter();
+            ep.Visit(expr);
+            Assert.That(ep.ToString(), Is.EqualTo("(2+3)*4"));
+        }
+    }
+}
